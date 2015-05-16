ec1d6ed5fd86f3a6a440ed1c27a3974a956ae82b
Sunny Mittal <sunnymittal2003@gmail.com>
2015-05-15 21:22:12 -0700

    refactor case statement

--- brain.rb
+++ brain.rb
@@ -1,27 +1,30 @@
 class Brain
   def self.fuck(file)
-    initialize file
-    i, len = 0, @contents.size
-    while i < len
-      case @contents[i]
-      when '>' then @ptr += 1
-      when '<' then @ptr -= 1
-      when '+' then @data[@ptr] += 1
-      when '-' then @data[@ptr] -= 1
-      when '.' then print @data[@ptr].chr
-      when '[' then @loop_start = i
-      when ']' then i = @loop_start if @data[@ptr] > 0
-      else  p 'invalid character'
-      end
-      i += 1
+    init file
+    setup_ops
+    len = @contents.size
+    while @pos < len
+      @ops[@contents[@pos]].call
+      @pos += 1
     end
     puts
   end
 
   private
 
-  def self.initialize(file)
-    @ptr, @loop_start = 0, 0
+  def self.setup_ops
+    @ops = {}
+    @ops['>'] = ->{ @ptr += 1 }
+    @ops['<'] = ->{ @ptr -= 1 }
+    @ops['+'] = ->{ @data[@ptr] += 1 }
+    @ops['-'] = ->{ @data[@ptr] -= 1 }
+    @ops['.'] = ->{ print @data[@ptr].chr }
+    @ops['['] = ->{ @loop_start = @pos }
+    @ops[']'] = ->{ @pos = @loop_start if @data[@ptr] > 0 }
+  end
+
+  def self.init(file)
+    @ptr, @pos, @loop_start = 0, 0, 0
     @data = Array.new(100) { 0 }
     @contents = File.open(file) do |f|
       f.each_line.to_a.reduce('') { |acc, line| acc + line.chomp }
